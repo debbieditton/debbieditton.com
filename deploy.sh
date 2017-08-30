d0a53f1029c37088c661e57b83cebda28ab765c6
dittonjs <jditton.atomic@gmail.com>
2017-08-30 08:34:05 -0600

    fix little bug

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 cd docs
+echo "www.debbieditton.com" >> CNAME
 git init
 git remote add origin git@github.com:debbiedittonvoice/debbiedittonvoice.github.io.git
 git add .
 git commit -m 'deploy'
-git push -u origin master
+git push -u origin master --force
